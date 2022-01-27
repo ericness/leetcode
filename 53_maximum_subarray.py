768807f3e77ba9014e962cfa0b33405b4f6e78be
ericness <eric.christopher.ness@gmail.com>
2022-01-27 17:47:27 -0600

    53 O(N) attempt 2

--- 53_maximum_subarray.py
+++ 53_maximum_subarray.py
@@ -18,14 +18,36 @@ class Solution:
         Returns:
             int: Sum of max subarray
         """
-        max_sum = -10001
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if j > i:
-                    subarray_sum = sum(nums[i:j])
-                    if subarray_sum > max_sum:
-                        max_sum = subarray_sum
-        return max_sum
+        array_size = len(nums)
+        left_current = 0
+        left_min = -10001
+        left_index = 0
+        right_current = 0
+        right_min = -10001
+        right_index = 0
+        for i in range(array_size):
+            if nums[i] + left_current > left_min:
+                left_max = left_current + nums[i]
+                left_index = i
+
+            left_current += nums[i]
+
+        for i in range(array_size):
+            j = array_size - i - 1
+            if nums[j] + left_current > left_max:
+                left_max = left_current + nums[j]
+                left_index = j
+
+            left_current += nums[j]
+
+        return right_max - left_max
 
 
 # @lc code=end
+
+if __name__ == "__main__":
+    sol = Solution()
+    result = sol.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+    result = sol.maxSubArray([1, 1, -10, 2, 2])
+    # result = sol.maxSubArray([-4, -3, -2, -1])
+    print(result)
