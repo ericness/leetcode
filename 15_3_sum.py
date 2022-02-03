c39dfaabda6fb17327233d1aa09a6afdb1e8dc15
ericness <eric.christopher.ness@gmail.com>
2022-02-03 17:14:37 -0600

    15 brute force

---
+++ 15_3_sum.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        """Find unique triplets that sum to zero.
+
+        Args:
+            nums (List[int]): List of numbers
+
+        Returns:
+            List[List[int]]: List of triplets that sum to zero
+
+        """
+        result = []
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        triplet = sorted([nums[i], nums[j], nums[k]])
+                        if triplet not in result:
+                            result.append(triplet)
+        return result
+
+
+# @lc code=end
