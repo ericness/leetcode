20ea79b78ad0d02ed9061f2e58b61544963115cb
ericness <eric.christopher.ness@gmail.com>
2022-03-02 18:44:30 -0600

    424 first shot at calculate_length

--- 424_longest_repeating_character_replacement.py
+++ 424_longest_repeating_character_replacement.py
@@ -6,6 +6,7 @@
 
 # @lc code=start
 from collections import defaultdict
+from typing import List
 
 
 class Solution:
@@ -37,9 +38,40 @@ class Solution:
 
         return 0
 
+    def calculate_length(self, diffs: List[int], k: int) -> int:
+        """Calculate maximum sequence length based
+        on the differences between character positions
+
+        Args:
+            diffs (List[int]): List of position differences
+            k (int): Number of characters to replace
+
+        Returns:
+            int: Maximum length of replacement
+        """
+        behind, ahead = 0, 0
+        current_k = diffs[0]
+        max_length = 0
+
+        while ahead < len(diffs):
+            if current_k <= k:
+                ahead += 1
+                if ahead == len(diffs):
+                    break
+                current_k += diffs[ahead]
+            else:
+                behind += 1
+                current_k -= diffs[behind]
+            length = ahead - behind + current_k
+            if length > max_length and current_k <= k:
+                max_length = length
+
+        return max_length
+
 
 # @lc code=end
 if __name__ == "__main__":
     sol = Solution()
-    res = sol.characterReplacement("aabbcba", 3)
+    # res = sol.characterReplacement("aabbcba", 3)
+    res = sol.calculate_length([1, 2, 1], 2)
     print(res)
