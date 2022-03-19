1910fc0ebe2cbffa65be6882e6606cc5b5f30fe6
ericness <eric.christopher.ness@gmail.com>
2022-03-19 14:12:18 -0500

    200 breadth first search

--- 200_number_of_islands.py
+++ 200_number_of_islands.py
@@ -5,7 +5,19 @@
 #
 
 # @lc code=start
-from typing import List
+import collections
+from enum import Enum
+from typing import List, Set, Tuple
+
+Node = collections.namedtuple("Node", "row col")
+Direction = collections.namedtuple("Direction", "rows_down cols_right")
+
+
+class Directions(Enum):
+    Up = Direction(-1, 0)
+    Down = Direction(1, 0)
+    Left = Direction(0, -1)
+    Right = Direction(0, 1)
 
 
 class Solution:
@@ -18,25 +30,35 @@ class Solution:
         Returns:
             int: Number of islands
         """
-        current_island = 2
+        row_count = len(grid)
+        col_count = len(grid[0])
+        visited = set()
+        number_islands = 0
 
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
+        for row in range(row_count):
+            for col in range(col_count):
                 if grid[row][col] == "1":
                     mark_island(grid, row, col, str(current_island))
                     current_island += 1
-        return current_island - 2
+        return number_islands
 
 
-def mark_island(grid: List[List[str]], row: int, col: int, island_number: str):
-    """Recursively mark all parts of an island with island_number.
+def breadth_first_search(
+    grid: List[List[str]], visited: Set[Tuple[int, int]], row: int, col: int
+) -> Set[Tuple[int, int]]:
+    """Perform breadth first search on specified grid element
 
     Args:
         grid (List[List[str]]): Grid with 0s and 1s
+        visited (Set[Tuple[int, int]]): Set of visited nodes
         row (int): Grid row
         col (int): Grid column
-        island_number (str): Number to mark the island with
+    Returns:
+        Set[Tuple[int, int]]: Set of nodes visited by function
     """
+    node_queue = collections.deque()
+    node_queue.append((row, col))
+
     if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]):
         return
     elif grid[row][col] == "0":
