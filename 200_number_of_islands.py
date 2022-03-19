947f89732fb06e8b06f9a54c5f17a1023063a20a
ericness <eric.christopher.ness@gmail.com>
2022-03-19 18:43:36 -0500

    200 breadth first search contd

--- 200_number_of_islands.py
+++ 200_number_of_islands.py
@@ -12,12 +12,12 @@ from typing import List, Set, Tuple
 Node = collections.namedtuple("Node", "row col")
 Direction = collections.namedtuple("Direction", "rows_down cols_right")
 
-
-class Directions(Enum):
-    Up = Direction(-1, 0)
-    Down = Direction(1, 0)
-    Left = Direction(0, -1)
-    Right = Direction(0, 1)
+DIRECTIONS = [
+    Direction(-1, 0),
+    Direction(1, 0),
+    Direction(0, -1),
+    Direction(0, 1),
+]
 
 
 class Solution:
@@ -37,41 +37,49 @@ class Solution:
 
         for row in range(row_count):
             for col in range(col_count):
-                if grid[row][col] == "1":
-                    mark_island(grid, row, col, str(current_island))
-                    current_island += 1
+                node = Node(row, col)
+                if grid[node.row][node.col] == "1":
+                    breadth_first_search(grid, visited, node)
+                    number_islands += 1
         return number_islands
 
 
 def breadth_first_search(
-    grid: List[List[str]], visited: Set[Tuple[int, int]], row: int, col: int
+    grid: List[List[str]], visited: Set[Tuple[int, int]], node: Node
 ) -> Set[Tuple[int, int]]:
     """Perform breadth first search on specified grid element
 
     Args:
         grid (List[List[str]]): Grid with 0s and 1s
         visited (Set[Tuple[int, int]]): Set of visited nodes
-        row (int): Grid row
-        col (int): Grid column
+        node (Node): Node to search
     Returns:
         Set[Tuple[int, int]]: Set of nodes visited by function
     """
+    row_count = len(grid)
+    col_count = len(grid[0])
+    local_visited = set()
     node_queue = collections.deque()
-    node_queue.append((row, col))
+    node_queue.append(node)
 
-    if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]):
-        return
-    elif grid[row][col] == "0":
-        return
-    elif grid[row][col] == "1":
-        grid[row][col] = island_number
-        mark_island(grid, row - 1, col, island_number)
-        mark_island(grid, row, col - 1, island_number)
-        mark_island(grid, row + 1, col, island_number)
-        mark_island(grid, row, col + 1, island_number)
-        return
-    else:
-        return
+    while node_queue:
+        search_node = node_queue.popleft()
+        for direction in DIRECTIONS:
+            new_node = Node(
+                search_node.row + direction.rows_down,
+                search_node.col + direction.cols_right,
+            )
+
+            if (
+                0 < new_node.row < row_count
+                and 0 < new_node.col < col_count
+                and new_node not in visited
+                and new_node not in local_visited
+                and grid[new_node.row][new_node.col] == "1"
+            ):
+                node_queue.append(new_node)
+            local_visited.add(new_node)
+    return local_visited
 
 
 # @lc code=end
