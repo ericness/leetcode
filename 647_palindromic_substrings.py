17269e8cb17bf94c336c4293ea9fbd74774a5226
ericness <eric.christopher.ness@gmail.com>
2022-04-01 19:15:53 -0500

    647 optimal solution

--- 647_palindromic_substrings.py
+++ 647_palindromic_substrings.py
@@ -17,9 +17,16 @@ class Solution:
         """
         palindromes = 0
         for i in range(len(s)):
-            for j in range(i, len(s)):
-                if s[i:j] == s[j:i:-1]:
-                    palindromes += 1
+            left, right = i, i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                palindromes += 1
+                left -= 1
+                right += 1
+            left, right = i, i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                palindromes += 1
+                left -= 1
+                right += 1
         return palindromes
 
 
